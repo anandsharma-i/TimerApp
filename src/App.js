8cc646c71eadc1a3312943762673e56289a104f6
anandsharma-i <anandrajsharma1@gmail.com>
2022-11-18 18:25:46 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Player from "./Player";
+import Timer from "./Timer";
+
+function App() {
+  return (
+    <Timer/>
+  );
+}
+
+export default App;
---
+++ src/Break.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Break = () => {
+  return (
+    <div>
+      <p>Bhai break le le na ab.</p>
+    </div>
+  )
+}
+
+export default Break
